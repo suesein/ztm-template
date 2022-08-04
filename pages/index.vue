c6d8afb317c8f01fbec7056fbbec08d9ce7b3761
suesein <sueseinlai@gmail.com>
2022-08-04 20:52:59 +0800

    updated homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
+      <h1 class="text-5xl">Welcome to Will Trade!</h1>
       <p class="mt-5">Turn your ideas into working web applications.</p>
     </div>
   </div>
 </template>
+
